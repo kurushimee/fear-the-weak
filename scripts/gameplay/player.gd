191f2d3e4e71ffa4e4d17d43690cf25724992de7
kurushimee <jermacoff@gmail.com>
2025-03-09 13:25:37 +0700

    Initial commit

---
+++ scripts/gameplay/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+
+
+func _physics_process(_delta: float) -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var direction := Input.get_vector(&"move_left", &"move_right", &"move_up", &"move_down").normalized()
+	if direction:
+		velocity = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()

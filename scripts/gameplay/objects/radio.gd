ce4fd14a494282daaf5a1b1248e10d7f450cb2b3
kurushimee <jermacoff@gmail.com>
2025-03-12 20:59:12 +0700

    feat: make radio fullscreen

--- scripts/gameplay/objects/radio.gd
+++ scripts/gameplay/objects/radio.gd
@@ -2,7 +2,7 @@ class_name Radio
 extends StaticBody2D
 
 @onready var interactable: Interactable = $Interactable
-@onready var minigame_ui: RadioMinigameUI = $RadioMinigameUI
+@onready var minigame_ui: RadioMinigameUI = $CanvasLayer/RadioMinigameUI
 
 
 func _on_interacted() -> void:
